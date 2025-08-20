d38e4d5cb501136c7d47939e2e7e9e6986c0b9e1
Shubham Misal <shubhammisal2710@gmail.com>
2025-08-20 13:39:40 +0530

    Token 6

--- jwt.js
+++ jwt.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const jwtAuthMiddleware=(req, res, next)=>{
 
-    const token = req.headers.authorization.split(' ')[0];
+    const token = req.headers.authorization.split(' ')[1];
     if(!token) return res.status(401).json({error:'Unarthorized'})
 
         try{
